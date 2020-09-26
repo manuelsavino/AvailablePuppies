39efc5fc76e8bbc4b844a4acb030ad7aafbe257a
manuelsavino <manuelsavino@gmail.com>
2020-09-26 15:23:19 -0400

    add puppy details page

--- src/pages/puppyDetails.js
+++ src/pages/puppyDetails.js
@@ -11,54 +11,60 @@ import {
 
 export default function puppyDetails(props) {
   const pup = props.location.state
-  const imageUrl = pup.Photo
-    ? `${pup.Photo.BaseUrl}${pup.Photo.Size500}`
-    : `https://www.petlandflorida.com/wp-content/themes/petland/assets/images/no-available.png`
-  return (
-    <Layout>
-      <SEO title="Home" />
-      <Hero height="200px" />
-      {console.log()}
-      <Container>
-        <PuppyDetailsContainer>
-          <PuppyDetailsImage src={imageUrl} />
-          <PuppyDetailsInfo>
-            <h2>{pup.PetName}</h2>
-            <div>
-              <h3>Details</h3>
-              <ul>
-                <li>
-                  <span>Breed</span>
-                  <span>{pup.BreedName}</span>
-                </li>
-                <li>
-                  <span>Color</span>
-                  <span>{pup.Coloring}</span>
-                </li>
-                <li>
-                  <span>Location</span>
-                  <span>
-                    {pup.Location.charAt(0).toUpperCase() +
-                      pup.Location.slice(1)}
-                  </span>
-                </li>
-                <li>
-                  <span>Gender</span>
-                  <span>{pup.Gender}</span>
-                </li>
-                <li>
-                  <span>DOB</span>
-                  <span>{pup.BirthDate}</span>
-                </li>
-                <li>
-                  <span>Ref ID</span>
-                  <span>{pup.PetId}</span>
-                </li>
-              </ul>
-            </div>
-          </PuppyDetailsInfo>
-        </PuppyDetailsContainer>
-      </Container>
-    </Layout>
-  )
+  if (pup) {
+    return (
+      <Layout>
+        <SEO title="Home" />
+        <Hero height="200px" />
+        <Container>
+          <PuppyDetailsContainer>
+            <PuppyDetailsImage
+              src={
+                pup.Photo
+                  ? `${pup.Photo.BaseUrl}${pup.Photo.Size500}`
+                  : `https://www.petlandflorida.com/wp-content/themes/petland/assets/images/no-available.png`
+              }
+            />
+            <PuppyDetailsInfo>
+              <h2>{pup.PetName}</h2>
+              <div>
+                <h3>Details</h3>
+                <ul>
+                  <li>
+                    <span>Breed</span>
+                    <span>{pup.BreedName}</span>
+                  </li>
+                  <li>
+                    <span>Color</span>
+                    <span>{pup.Coloring}</span>
+                  </li>
+                  <li>
+                    <span>Location</span>
+                    <span>
+                      {pup.Location.charAt(0).toUpperCase() +
+                        pup.Location.slice(1)}
+                    </span>
+                  </li>
+                  <li>
+                    <span>Gender</span>
+                    <span>{pup.Gender}</span>
+                  </li>
+                  <li>
+                    <span>DOB</span>
+                    <span>{pup.BirthDate}</span>
+                  </li>
+                  <li>
+                    <span>Ref ID</span>
+                    <span>{pup.PetId}</span>
+                  </li>
+                </ul>
+              </div>
+            </PuppyDetailsInfo>
+          </PuppyDetailsContainer>
+        </Container>
+      </Layout>
+    )
+  } else {
+    return <h1>Something went wrong...</h1>
+  }
 }
